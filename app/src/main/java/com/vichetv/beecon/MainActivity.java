491167014d9c447acf8575dd05b62f9068553dbe
Victoria <victoria.vichet@gmail.com>
2020-03-11 16:14:27 +0100

    devices trouvés non affichés

--- app/src/main/java/com/vichetv/beecon/MainActivity.java
+++ app/src/main/java/com/vichetv/beecon/MainActivity.java
@@ -1,19 +1,31 @@
 package com.vichetv.beecon;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends Activity  {
@@ -34,6 +46,12 @@ public class MainActivity extends Activity  {
 
         BA = BluetoothAdapter.getDefaultAdapter();
         lv = (ListView)findViewById(R.id.listView);
+
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+
     }
 
     public void on(View v){
@@ -69,5 +87,77 @@ public class MainActivity extends Activity  {
         final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
 
         lv.setAdapter(adapter);
+    }
+
+    public void listNew(View v){
+        BroadcastReceiver mReceiver;
+        final List<String> devices= new ArrayList<>();
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    String deviceInfo=device.getName() + "\n" + device.getAddress();
+                    devices.add(deviceInfo);
+                    Log.i("In action_found", "A device found");
+                }
+                else if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                    Log.i("In discovery started", "Discovery started");
+                }
+                else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    Log.i("In discovery finished", "Discovery finished");
+                }
+            }
+        };
+
+
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
+
+
+
+        if(BA.isEnabled())
+            Log.i("Status", "Bluetooth enabled");
+        if(BA.isDiscovering())
+            BA.cancelDiscovery();
+        BA.startDiscovery();
+        System.out.println("test1");
+        if(BA.isDiscovering()) {
+            System.out.println("test2");
+            Toast.makeText(getApplicationContext(), "Discovering",Toast.LENGTH_LONG).show();
+
+        }
+
+        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, devices);
+        lv.setAdapter(adapter);
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+
+    @Override
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        try{
+            if(receiver!=null)
+                unregisterReceiver(receiver);
+        }catch(Exception e)       {
+        }
+        super.onDestroy();
     }
 }
